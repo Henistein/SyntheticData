e9963c052ce70578943fef29fafe33f6596f8b72
Henistein <henriquejesus20@gmail.com>
2022-08-16 20:20:08 +0100

    ProdList

--- src/data_gen.py
+++ src/data_gen.py
@@ -4,6 +4,28 @@ from math import prod, radians
 from random import shuffle, choice
 from copy import deepcopy
 
+class ListStruct:
+  def __init__(self, lst, total_combs):
+    self.lst = lst
+    self.elements = {k:total_combs/len(lst) for k in lst}
+  
+  def request_element(self):
+    if len(self.lst) == 0: return None
+    element = choice(self.lst)
+    self.elements[element] -= 1
+    if self.elements[element] == 0: self.lst.remove(element)
+    return element
+
+class ListProd:
+  def __init__(self, lists):
+    self.total_combs = prod([len(x) for x in lists])
+    self.lists = [ListStruct(lst, self.total_combs) for lst in lists]
+  
+  def request_data(self, ammount):
+    assert ammount >= 1
+    assert ammount <= self.total_combs, 'Not enough combinations!'
+    return [[lst.request_element() for lst in self.lists] for _ in range(ammount)]
+
 class ObjFeatures:
   def __init__(self, obj):
     self.obj = obj
@@ -41,11 +63,8 @@ class DataGen:
   
   def generate(self, ammount):
     assert ammount <= self.all_combinations, 'Not enough combinations!'
-
-    s = set()
-    while len(s) < ammount:
-      s.add(tuple([choice(lst) for obj in self.objs.values() for lst in obj.features.values()])) # not the most efficient way, but it works
-    return s
+    prod_list = ListProd([lst for obj in self.objs.values() for lst in obj.features.values()])
+    return prod_list.request_data(ammount)
 
 
 def _setattr(obj, attr_s, val):
@@ -53,8 +72,6 @@ def _setattr(obj, attr_s, val):
   for part in parts[:-1]:
     obj = getattr(obj, part)
   setattr(obj, parts[-1], val)
-
-
 
 class CreateData(DataGen):
   def __init__(self, blender, destination_path):
@@ -74,8 +91,6 @@ class CreateData(DataGen):
         # save image
         self.blender.context.scene.render.filepath = f'{self.destination_path}/img{str(1000000+i)[1:]}.png' 
         self.blender.ops.render.render(write_still=True)
-        if i % 1000 == 0:
-          print(f'{i}/25000')
         i += 1
 
   def create_csv(self):
