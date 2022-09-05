2af0995cf80a935213f6b8e1052794972257ad7a
Henistein <henriquejesus20@gmail.com>
2022-09-05 10:44:42 +0100

    Day Night Cycle

--- data_gen/main.py
+++ data_gen/main.py
@@ -12,9 +12,13 @@ import data_gen
 import importlib
 importlib.reload(data_gen)
 
+# Choose Cycles and GPU
+#bpy.data.scenes[0].render.engine = "CYCLES"
+#bpy.context.scene.cycles.device = "GPU"
 
-# remove cube object
+# remove and light cube object
 bpy.data.objects['Cube'].select_set(True)
+bpy.data.objects['Light'].select_set(True)
 bpy.ops.object.delete()
 
 # import sign object
@@ -54,6 +58,38 @@ bpy.ops.object.constraint_add(type='FOLLOW_PATH')
 bpy.data.objects['Empty'].constraints['Follow Path'].target = bpy.data.objects['BezierCircle']
 
 
+# Day night cycle
+
+# 1 - Change render engine to cycles and device set to GPU
+bpy.data.scenes[0].render.engine = "CYCLES"
+bpy.context.scene.cycles.device = "GPU"
+
+# 2 - World properties, change world surface color to sky texture
+sky_texture = bpy.context.scene.world.node_tree.nodes.new("ShaderNodeTexSky")
+bg = bpy.context.scene.world.node_tree.nodes["Background"]
+bpy.context.scene.world.node_tree.links.new(bg.inputs["Color"], sky_texture.outputs["Color"])
+
+# 3 - Add a sun and set strength to 0
+bpy.ops.object.light_add(type='SUN')
+bpy.data.lights['Sun'].energy = 0
+
+# 4 - World properties, Sun Position, set Sun Object to 'Sun' and Sky Texture
+bpy.context.scene.sun_pos_properties.sun_object = bpy.data.objects["Sun"]
+bpy.context.scene.sun_pos_properties.sky_texture = "Sky Texture"
+
+# 5 - Change coordinates to become more realistic (optional)
+bpy.context.scene.sun_pos_properties.co_parser = "38.691629133197814, -9.215922843598607"
+
+# 6 - Change month and day of the year
+bpy.context.scene.sun_pos_properties.month = 6
+bpy.context.scene.sun_pos_properties.day = 20
+
+# Reduce samples (faster rendering)
+bpy.context.scene.cycles.samples = 1024
+
+
+
+
 if 1 == 1:
   path = '/media/henistein/Novo volume/SyntheticData/teste'
 
@@ -71,6 +107,10 @@ if 1 == 1:
   dg.add_feature("location.x", -40, 0, 4)
   dg.add_feature("location.y", -20, 20, 4)
   dg.add_feature("location.z", -10, 10, 2)
+  # day night cycle
+  dg.add_obj("scene", bpy.context.scene)
+  dg.add_feature("sun_pos_properties.time", 5, 20, 1)
+
 
   dg.generate(200)
   dg.create_data()
--- train.py
+++ train.py
@@ -4,45 +4,61 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+import matplotlib.pyplot as plt
 from tqdm import tqdm 
 from dataset import load_train_dataset, load_val_dataset
-from torchvision.models import efficientnet_v2_l
+from torchvision.models import efficientnet_v2_s
 
 class Train:
-  EPOCHS = 400
+  EPOCHS = 3
   LEARNING_RATE = 1e-3
 
   def __init__(self, train_dataset, val_dataset):
-    self.model = efficientnet_v2_l(weights=None, pretrained=True)
+    self.model = efficientnet_v2_s(weights=None, pretrained=True)
     # Hack to get the model to work with 2 output classes
     self.model.classifier = nn.Sequential(
       nn.Dropout(p=0.4, inplace=True),
       nn.Linear(in_features=1280, out_features=2, bias=True),
     )
-    self.model.cuda().train()
+    self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    self.model.to(self.device)
     self.train_dataset = train_dataset
     self.val_dataset = val_dataset 
     self.optimizer = optim.Adam(self.model.parameters(), lr=Train.LEARNING_RATE)
     self.criterion = nn.CrossEntropyLoss()
   
+  def _mean(self, x):
+    return sum(x)/len(x)
+  
   def _train(self):
+    self.model.train()
+    train_loss, train_acc = [], []
+    i = 0
     for x,y in (t:=tqdm(self.train_dataset)):
-      x, y = x.cuda(), y.cuda()
+      x, y = x.to(self.device), y.to(self.device)
       self.optimizer.zero_grad()
       # Forward pass
       outputs = self.model(x)
       # Compute loss
       loss = self.criterion(outputs, y)
+      # Calculate the accuracy and save it
+      predicted = torch.argmax(outputs, dim=1)
+      train_acc.append((predicted == y).float().mean().item())
+      train_loss.append(loss.item())
       # Backpropagation
       loss.backward()
       # Update weights
       self.optimizer.step()
       t.set_description(f'loss: {loss.item():.4f}')
+      if i == 2:
+        break
+      i+=1
+    return self._mean(train_acc), self._mean(train_loss)
   
   def _validate(self):
     self.model.eval()
-    val_running_loss = 0.0
-    val_running_correct = 0.0
+    val_loss, val_acc = [], []
+    i = 0
     with torch.no_grad():
       for x,y in tqdm(self.val_dataset):
         x, y = x.cuda(), y.cuda()
@@ -50,26 +66,50 @@ class Train:
         outputs = self.model(x)
         # Compute loss
         loss = self.criterion(outputs, y)
-        val_running_loss += loss.item()
         # Calculate accuracy
-        _, predicted = torch.max(outputs.data, 1)
-        val_running_correct += (predicted == y).sum().item()
-    epoch_loss = val_running_loss / len(self.val_dataset)
-    epoch_acc = 100.0 * val_running_correct / len(self.val_dataset)
-    return epoch_loss, epoch_acc
+        predicted = torch.argmax(outputs, dim=1)
+        val_acc.append((predicted == y).float().mean().item())
+        val_loss.append(loss.item())
+        if i == 2:
+          break
+        i+=1
+    return self._mean(val_acc), self._mean(val_loss)
   
-  def train(self):
+  def compute_acc_loss(self, t_acc, v_acc, t_loss, v_loss):
+    print(t_acc, v_acc, t_loss, v_loss)
+    plt.plot(t_acc)
+    plt.plot(v_acc)
+    plt.plot(t_loss)
+    plt.plot(v_loss)
+    plt.savefig('results.png')
+
+  def train(self, path):
     valid_acc, valid_loss = [], []
+    train_acc, train_loss = [], []
+    best_acc = 0
 
     for epoch in range(Train.EPOCHS):
-      self._train()
+      # train
+      epoch_loss, epoch_acc = self._train()
+      train_loss.append(epoch_loss)
+      train_acc.append(epoch_acc)
+      # validation
       epoch_loss, epoch_acc = self._validate()
       valid_loss.append(epoch_loss)
       valid_acc.append(epoch_acc)
+
+      if epoch_acc > best_acc:
+        # save the best model
+        torch.save(self.model.state_dict(), path+"/best.pt")
+      # save the last model 
+      torch.save(self.model.state_dict(), path+"/last.pt")
+
       print(f'Epoch: {epoch}, Loss: {epoch_loss:.4f}, Acc: {epoch_acc:.4f}')
+    
+    self.compute_acc_loss(t_acc=train_acc, v_acc=valid_acc, t_loss=train_loss, v_loss=valid_loss)
 
 
 if __name__ == '__main__':
-  BS = 2
+  BS = 14
   train = Train(train_dataset=load_train_dataset(BS), val_dataset=load_val_dataset(BS))
-  train.train()
+  train.train(path='/home/henistein/programming/UBI/SyntheticData/results')
